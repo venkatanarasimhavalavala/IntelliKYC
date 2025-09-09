1b13514b3bc6a092b3260e6b387adaa882ec348a
VALAVALA VENKATA NARASIMHA <159681684+venkatanarasimhavalavala@users.noreply.github.com>
2025-09-09 18:18:45 +0530

    Update MailScript_get_backend_task_link.js

--- src/notifications/MailScript_get_backend_task_link.js
+++ src/notifications/MailScript_get_backend_task_link.js
@@ -1,1 +1,4 @@
-
+(function runMailScript(current, template, email, email_action, event) {
+  var url = '<a href="' + gs.getProperty('glide.servlet.uri') + 'nav_to.do?uri=' + current.getTableName() + '.do?sys_id=' + current.sys_id + '">Click here to open the task</a>';
+  template.print(url);
+})(current, template, email, email_action, event);
