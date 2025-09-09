76c8ac033917670dcdeb52d5f07c195076c52ebf
VALAVALA VENKATA NARASIMHA <159681684+venkatanarasimhavalavala@users.noreply.github.com>
2025-09-09 18:18:14 +0530

    Update MailScript_get_kyc_portal_link.js

--- src/notifications/MailScript_get_kyc_portal_link.js
+++ src/notifications/MailScript_get_kyc_portal_link.js
@@ -1,1 +1,6 @@
 
+(function runMailScript(current, template, email, email_action, event) {
+  var portalSuffix = gs.getProperty('x_snc_kyc_automati.portal_suffix', 'sp');
+  var url = '<a href="' + gs.getProperty('glide.servlet.uri') + portalSuffix + '?id=ticket&table=' + current.getTableName() + '&sys_id=' + current.sys_id + '">Click here to view your application</a>';
+  template.print(url);
+})(current, template, email, email_action, event);
