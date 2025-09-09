35aa5729dfd49262ae3f45d64ef7ee2fc217f2d1
VALAVALA VENKATA NARASIMHA <159681684+venkatanarasimhavalavala@users.noreply.github.com>
2025-09-09 18:19:26 +0530

    Update VA_Submit_Application_Create_Record_Script.js

--- src/virtual_agent/VA_Submit_Application_Create_Record_Script.js
+++ src/virtual_agent/VA_Submit_Application_Create_Record_Script.js
@@ -1,1 +1,22 @@
 
+(function(vaInputs, vaVars) {
+    var kycGr = new GlideRecord('x_snc_kyc_automati_kyc_application');
+    kycGr.initialize();
+    kycGr.setValue('customer', vaInputs.user.sys_id);
+    kycGr.setValue('applicant_type', vaInputs.applicant_type);
+    kycGr.setValue('pan', vaInputs.pan);
+    kycGr.setValue('aadhaar', vaInputs.aadhaar);
+    kycGr.setValue('account_type', vaInputs.account_type);
+    kycGr.setValue('pep_flag', vaInputs.pep_flag);
+    if (vaInputs.applicant_type == 'Individual') {
+        kycGr.setValue('declared_income_level', vaInputs.income_level);
+    } else {
+        kycGr.setValue('industry', vaInputs.industry);
+    }
+    kycGr.setValue('status', 'submitted');
+    var recordSysId = kycGr.insert();
+    var recordNumber = kycGr.getValue('number');
+
+    vaVars.new_kyc_number = recordNumber;
+    vaVars.new_kyc_sysid = recordSysId;
+})(vaInputs, vaVars);
