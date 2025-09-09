4bf3487e18f42b8691126b95c1278d2b33c956eb
VALAVALA VENKATA NARASIMHA <159681684+venkatanarasimhavalavala@users.noreply.github.com>
2025-09-09 18:17:13 +0530

    Update Request_More_Info.js

--- src/ui_actions/Request_More_Info.js
+++ src/ui_actions/Request_More_Info.js
@@ -1,1 +1,11 @@
+var kycApp = current.kyc_app.getRefRecord();
+kycApp.setValue('status', 'awaiting_info');
+kycApp.work_notes = "Additional information requested by " + gs.getUserDisplayName();
+kycApp.update();
 
+gs.eventQueue('x_snc_kyc_automati.awaiting_info', kycApp, gs.getUserID(), '');
+
+current.state = 4; // 4 = Closed Incomplete
+current.update();
+
+action.setRedirectURL(kycApp);
