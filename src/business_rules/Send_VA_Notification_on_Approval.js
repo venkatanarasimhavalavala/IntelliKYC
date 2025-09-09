8edf548b995496a2abce0d9a7af9eb050c62a4f5
VALAVALA VENKATA NARASIMHA <159681684+venkatanarasimhavalavala@users.noreply.github.com>
2025-09-09 18:16:13 +0530

    Update Send_VA_Notification_on_Approval.js

--- src/business_rules/Send_VA_Notification_on_Approval.js
+++ src/business_rules/Send_VA_Notification_on_Approval.js
@@ -1,1 +1,12 @@
+(function executeRule(current, previous) {
+    var userId = current.getValue('customer');
+    var appNumber = current.getValue('number');
+    var message = 'Hi! Great news, your KYC application ' + appNumber + ' has just been approved.';
+    
+    try {
+        new sn_va_as_service.VAService().sendVirtualAgentMessage('text', userId, message);
+    } catch (e) {
+        gs.error("Failed to send VA notification for KYC approval. Error: " + e);
+    }
 
+})(current, previous);
